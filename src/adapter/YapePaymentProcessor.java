69af528653b6819583d36d7e773fce0d3a5bccdf
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-05-23 11:59:44 -0500

    feat: Agregación de procesadores de pago para Tarjeta de Credito y Yape

---
+++ src/adapter/YapePaymentProcessor.java
@@ -0,0 +1,8 @@
+package adapter;
+
+public class YapePaymentProcessor implements PaymentProcessor{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Pago realizado con Yape:" + amount);
+    }
+}
