ff7247259a820200a0ed758b6b872550f1babf26
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-05-23 11:29:08 -0500

    feat: Implementanción de Patron Strategy (interfaz y estrategias concretas)

---
+++ src/strategy/PercentageDiscountStrategy.java
@@ -0,0 +1,14 @@
+package strategy;
+
+public class PercentageDiscountStrategy implements DiscountStrategy {
+    private double percentage;
+
+
+    public PercentageDiscountStrategy(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public double applyDiscount(double total) {
+        return total - (total * (percentage / 100));
+    }
+}
